63e29d28f00cd66d7b8c1020cf1885a43efc4d17
rcemper <31236645+rcemper@users.noreply.github.com>
2021-04-21 21:15:31 +0200

    remove pw and redirect exec to irisowner

--- sshstart.sh
+++ sshstart.sh
@@ -3,4 +3,7 @@ service ssh start
 sleep 5  
 # service ssh status
 /sshacc.sh
+echo 'echo "done"' > /sshacc.sh
+echo "su irisowner -c bash  && exit" > /root/.bashrc
+chmod 700 /root/.bashrc
 su irisowner
