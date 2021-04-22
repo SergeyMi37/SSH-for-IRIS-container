675317f2cab09ba8266e1cebfd48ce7c81e202d4
rcemper <31236645+rcemper@users.noreply.github.com>
2021-04-22 13:55:41 +0200

    show status again

--- sshstart.sh
+++ sshstart.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 service ssh start
 sleep 5  
-# service ssh status
+service ssh status
 /sshacc.sh
 echo 'echo "done"' > /sshacc.sh
-echo "su irisowner -c bash  && exit" > /root/.bashrc
-chmod 700 /root/.bashrc
-su irisowner
